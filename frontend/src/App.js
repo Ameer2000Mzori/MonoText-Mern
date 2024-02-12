cae3c1c00d31c63068f77f906d14a557c613c0d4
Ameer Ameen <ameerapex496@gmail.com>
2024-02-12 03:22:06 +0100

    small changes in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
