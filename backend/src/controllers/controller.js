e58aab697a2c11090d71d4302b04e3a23bb3bea0
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-12 21:55:23 +0100

    we added code to list our articles om controllers

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,7 +1,16 @@
+import { Article } from '../modules/article.js'
+
 export const homePage = (req, res) => {
   res.send('this message is from backend')
 }
 
 export const addArticle = (req, res) => {
-  res.send('thi is Article page')
+  Article.find()
+    .sort({
+      createAt: -1,
+    })
+    .then((result) => {
+      res.status(200).json(result)
+    })
+    .catch((err) => console.log(err))
 }
