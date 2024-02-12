9d0e6ccf93782f3a2b8ce649da73f9384347feb2
Ameer Ameen <ameerapex496@gmail.com>
2024-02-12 03:51:54 +0100

    added screens to tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      screens: {
+        xm: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
       colors: {
         primary: '#333333',
       },
