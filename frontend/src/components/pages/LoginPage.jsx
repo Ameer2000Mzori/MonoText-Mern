05efeaa6a60afd76798cafd911dd128bfe429844
Ameer Ameen <ameerapex496@gmail.com>
2024-02-12 05:11:10 +0100

    added function where the user name and email will go

--- frontend/src/components/pages/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage.jsx
@@ -4,6 +4,11 @@ export default function LoginPage() {
   const [userName, setUserName] = useState('')
   const [userEmail, setUserEmail] = useState('')
 
+  const loginComfirm = () => {
+    console.log('user name', userName)
+    console.log('user email', userEmail)
+  }
+
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col justify-center text-center items-center">
       <div className="grid grid-row-6 gap-2 w-[500px] h-[300px] bg-slate-500 p-4 text-start rounded-md text-white">
@@ -23,7 +28,9 @@ export default function LoginPage() {
           value={userEmail}
           onChange={(e) => setUserEmail(e.target.value)}
         />
-        <button type="button">submit</button>
+        <button type="button" onClick={loginComfirm}>
+          submit
+        </button>
         <p className="text-[0.9rem]">
           <b className=" underline text-blue-800"> New here? </b> Sign up now to
           get started!
