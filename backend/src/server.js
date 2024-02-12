56d05994f0dabf80d5a80092978589aa5b3c25ac
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-12 21:10:11 +0100

    added .env and changed port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import router from './routes/route.js'
+import 'dotenv/config'
 
 const app = express()
 app.use(router)
 
-app.listen(3000, (res, req) => {
-  console.log('your app is running at port 3000')
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, (res, req) => {
+  console.log(`your sever is running on ${PORT} port`)
 })
