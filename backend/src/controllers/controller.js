43b90b0a496b94cce602f7701b81b8285ea7f340
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-12 23:11:21 +0100

    added lost of code , finding the user articles

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -4,10 +4,12 @@ export const homePage = (req, res) => {
   res.send('this message is from backend')
 }
 
+// get articles
 export const getArticle = (req, res) => {
   Article.find()
+    .populate('author')
     .sort({
-      createAt: -1,
+      createdAt: -1,
     })
     .then((result) => {
       res.status(200).json(result)
@@ -15,48 +17,53 @@ export const getArticle = (req, res) => {
     .catch((err) => console.log(err))
 }
 
-export const createArticle = (req, res) => {
-  console.log('create article')
-  const { title, description, url } = req.body
-}
+// create article
 
-export const createVideo = (req, res) => {
-  const newVideo = new Video({
-    author: 'john',
+export const createArticle = async (req, res) => {
+  const { author, title, text } = req.body
+
+  const user = User.findById(author)
+
+  const newArticle = new Article({
+    author,
     title,
     text,
-    rate,
   })
-
-  newVideo
-    .save()
-    .then((result) => {
-      res.status(200).json({
-        message: 'Video saved successfully',
-        video: result,
-      })
+  try {
+    await newArticle.save()
+    user.articles.push(newArticle)
+    await user.save()
+    res.status(200).json({
+      message: 'user created successfully',
+      Article: result,
     })
-    .catch((err) => console.log(err))
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'An error occurred' })
+  }
 }
 
+// =======================================================================================
 //  user  list users acc
 
 export const getUsers = (req, res) => {
   User.find()
+    .populate('articles')
     .sort({
-      createAt: -1,
+      createdAt: -1,
     })
     .then((result) => {
+      console.log(result) // Check the result to see if articles are populated
       res.status(200).json(result)
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: 'An error occurred' })
+    })
 }
 
 // create user acc
 export const createAccount = (req, res) => {
-  console.log('create article')
-  console.log(req)
-  console.log(req.body)
   const { username, email, password } = req.body
 
   const newAccount = new User({
