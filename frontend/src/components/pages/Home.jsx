51b26267df1e296d0736fe669264adcfe6c8a649
Ameer Ameen <ameerapex496@gmail.com>
2024-02-12 04:33:37 +0100

    changes in wrap like cikirubg and responsvness

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-[100vh] w-[100vw] flex flex-row text-center items-center justify-center gap-4">
       {/* this is left side  */}
-      <div className="w-[10%] h-[80%] lg:flex hidden flex-col text-center items-center justify-center bg-sky-300 border-solid border-2 border-sky-500 ">
+      <div className="w-[15%] h-[80%] lg:flex hidden flex-col text-center items-center justify-center bg-sky-300 border-solid border-2 border-sky-500 ">
         <h1>this is left side</h1>
         <h1>10% / 80%</h1>
       </div>
@@ -14,7 +14,7 @@ export default function Home() {
         <h1>50% / 80%</h1>
       </div>
       {/* this is right side and mosly will be used for sugustions accounts */}
-      <div className="w-[10%] h-[80%] lg:flex hidden flex-col text-center items-center justify-center bg-sky-300 border-solid border-2 border-sky-500 ">
+      <div className="w-[15%] h-[80%] lg:flex hidden flex-col text-center items-center justify-center bg-sky-300 border-solid border-2 border-sky-500 ">
         <h1>this is Right side</h1>
         <h1>10% / 80%</h1>
       </div>
