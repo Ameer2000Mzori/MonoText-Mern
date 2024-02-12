5271f7c5e8964c20b2d451b82ba545a9b32c8e4a
Ameer Ameen <ameerapex496@gmail.com>
2024-02-12 03:52:12 +0100

    added notfound page

---
+++ frontend/src/components/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] flex flex-col text-center items-center justify-center bg-slate-400 ">
+      <h1>Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
