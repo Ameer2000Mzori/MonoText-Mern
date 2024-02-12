643acd77d66e273a284609f874b5290e499c9068
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-12 21:25:16 +0100

    connected db.js and files to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import router from './routes/route.js'
 import 'dotenv/config'
+import mongoConnect from './db/db.js'
 
 const app = express()
 app.use(router)
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, (res, req) => {
-  console.log(`your sever is running on ${PORT} port`)
+mongoConnect(() => {
+  app.listen(PORT, (req, res) => {
+    console.log(`server is running at port ${PORT}`)
+  })
 })
