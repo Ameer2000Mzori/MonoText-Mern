35c20602e77317137f93707770d9b19a0fee484f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-02 17:47:53 +0100

    added some comments in article controller for better understanding the code

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,16 +1,16 @@
 import { Article } from '../modules/article.js'
 import { User } from '../modules/users.js'
 import { Rate } from '../modules/rate.js'
+import { hashPassword, checkPwd } from '../utils/hashing.js'
+import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
-// this is the port to the server
 export const homePage = (req, res) => {
   res.send('this message is from backend')
 }
 
-// this is where you get all the articles
+// get articles
 export const getArticle = (req, res) => {
-  // getting all the articles
   Article.find()
     .populate('author')
     .populate('rates')
@@ -23,13 +23,13 @@ export const getArticle = (req, res) => {
     .catch((err) => console.log(err))
 }
 
-// this is where you create new articles
+// create article
 export const createArticle = async (req, res) => {
   const { title, text } = req.body
   console.log(req.user)
 
   try {
-    // finding user by id
+    // Find the user by ID
     const user = await User.findById(req.user.id)
 
     // Create a new article
@@ -45,7 +45,7 @@ export const createArticle = async (req, res) => {
     // Push the newly created article to the user's articles array
     user.articles.push(newArticle)
 
-    //  update the user
+    // Save the updated user
     await user.save()
 
     res.status(200).json({
@@ -58,11 +58,11 @@ export const createArticle = async (req, res) => {
   }
 }
 
-// here where you can rate an article
+// rate article
+
 export const rateArticle = async (req, res) => {
   const { value, article_id } = req.body
   try {
-    // finding the article by id
     const article = await Article.findById(article_id)
     const rate = await Rate.findOne({ article: article_id, user: req.user.id })
     if (rate) {
@@ -150,4 +150,91 @@ export const deleteArticle = async (req, res) => {
     console.log(err)
     res.status(500).json({ error: 'An error occurred' })
   }
+}
+
+// =======================================================================================
+//  user  list users acc
+
+export const getUsers = (req, res) => {
+  User.find()
+    .populate('articles')
+    .sort({
+      createdAt: -1,
+    })
+    .then((result) => {
+      console.log(result) // Check the result to see if articles are populated
+      res.status(200).json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: 'An error occurred' })
+    })
+}
+
+// create user acc
+export const createAccount = async (req, res) => {
+  const { username, email, password } = req.body
+  let newAccount
+  // console.log(req.headers.authorization.split(' ')[1])
+  try {
+    const user = await User.findOne({
+      $or: [{ username }, { email }],
+    })
+    if (user)
+      return res.status(400).json({
+        message: 'user already exist',
+      })
+    let new_pwd = await hashPassword(password)
+    newAccount = new User({
+      username,
+      email,
+      password: new_pwd,
+    })
+    await newAccount.save()
+    console.log(newAccount)
+    const token = jwt.sign({ id: newAccount._id }, process.env.SECRET, {
+      expiresIn: 3600,
+    }) // 1 hour
+    res.status(200).json({
+      message: 'user created successfully',
+      User: newAccount,
+      token,
+    })
+  } catch {
+    if (newAccount?._id) await newAccount.deleteOne({ _id: newAccount._id })
+    res.status(500).json({
+      message: 'Server failed',
+    })
+  }
+}
+
+// del user acc
+export const delAccount = async (req, res) => {
+  try {
+    const { password } = req.body
+    const user = await User.findById(req.user.id)
+    if (!user || !(await checkPwd(password, user.password)))
+      return res.status(400).json({ message: 'Password is wrong' })
+    await User.findOneAndDelete({ _id: req.user.id })
+    return res.status(200).json({ message: 'Account deleted successfully' })
+  } catch {
+    res.status(500).json({
+      message: 'Server failed',
+    })
+  }
+}
+
+// login
+export const login = async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  console.log(user)
+  if (user == null || !(await checkPwd(password, user.password)))
+    return res.status(400).json({ message: 'Username or Password is wrong ' })
+  const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+    expiresIn: 3600,
+  })
+  return res
+    .status(200)
+    .json({ message: 'logged in successfully ', data: user, token })
 }
