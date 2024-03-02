b13e723d3022e90f4c4a41e43095deb22b6726a8
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-02 17:25:27 +0100

    added user route

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -3,13 +3,9 @@ import {
   homePage,
   getArticle,
   createArticle,
-  createAccount,
-  login,
-  getUsers,
   rateArticle,
   deleteArticle,
   updateArticle,
-  delAccount,
 } from '../controllers/controller.js'
 import { guard } from '../middlewares/token.js'
 
@@ -22,11 +18,5 @@ router.post('/article', guard, createArticle)
 router.patch('/article', guard, updateArticle)
 router.delete('/article', guard, deleteArticle)
 router.post('/rate', guard, rateArticle)
-
-// users routes
-router.get('/user', getUsers)
-router.post('/user', createAccount)
-router.delete('/user/', guard, delAccount)
-router.post('/login', login)
 
 export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import router from './routes/route.js'
+import userRoute from './routes/userRoute.js'
 import 'dotenv/config'
 import mongoConnect from './db/db.js'
 
 const app = express()
 app.use(express.json())
 app.use(router)
+app.use(userRoute)
 
 const PORT = process.env.PORT || 4000
 
