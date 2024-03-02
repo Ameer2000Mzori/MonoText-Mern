6b806f3a3c150b9c4c7bfc722d25b7acf0e22028
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-02 18:08:01 +0100

    added effects to the buttons

--- frontend/src/components/pages/Home/shared/LoginLinks.jsx
+++ frontend/src/components/pages/Home/shared/LoginLinks.jsx
@@ -9,7 +9,10 @@ const AccountsLinks = () => {
   return (
     <ul className="flex flex-col gap-4">
       {pages.map((page) => (
-        <li className="w-[150px] h-[40px] bg-zinc-500" key={page.path}>
+        <li
+          className="w-[150px] h-[40px] bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-300"
+          key={page.path}
+        >
           <Link
             className="h-[100%] w-[100%] text-white flex flex-col text-center items-center justify-center"
             to={page.path}
